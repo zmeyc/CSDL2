3f4430efc25930cf3313bec5048579850c127a05
Andrey Fidrya <af@zabiyaka.com>
2016-10-01 14:55:39 +0300

    Add SDL_opengl

---
+++ shim.h
@@ -0,0 +1,14 @@
+#include "/usr/local/include/SDL2/SDL.h"
+#include "/usr/local/include/SDL2/SDL_image.h"
+#include "/usr/local/include/SDL2/SDL_mixer.h"
+#include "/usr/local/include/SDL2/SDL_net.h"
+#include "/usr/local/include/SDL2/SDL_syswm.h"
+#include "/usr/local/include/SDL2/SDL_ttf.h"
+#include "/usr/local/include/SDL2/SDL2_gfxPrimitives.h"
+#include "/usr/local/include/SDL2/SDL2_rotozoom.h"
+
+#define GL_GLEXT_PROTOTYPES
+#include "/usr/local/include/SDL2/SDL_opengl.h"
+
+int K_SDL_WINDOWPOS_UNDEFINED = SDL_WINDOWPOS_UNDEFINED;
+
